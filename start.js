775e23aca5b0400f5a852e4812ccf5cfa19d4b0f
Emmet <rehennessy3@gmail.com>
2019-12-08 00:21:41 -0500

    removed line of code referencing nonexistent file

--- start.js
+++ start.js
@@ -16,7 +16,6 @@ mongoose.connection
 
   
 require('./models/Keywords');
-require('./models/Documents');
 const app = require('./app');
 
 const server = app.listen(process.env.PORT || 5000, () => {
