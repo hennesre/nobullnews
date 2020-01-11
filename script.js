9af37b90f3e0b05e80026c9a5cab9575d631345d
Emmet <rehennessy3@gmail.com>
2020-01-11 08:54:51 -0500

    created scripts

---
+++ script.js
@@ -0,0 +1,47 @@
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('c89d944198e843f1892491c6e51896ce');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.Promise = global.Promise;
+mongoose.connection
+  .on('connected', () => {
+    console.log(`Mongoose connection open on ${process.env.DATABASE}`);
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+});
+
+var today = new Date();
+var lastWeek = new Date();
+lastWeek.setDate(lastWeek.getDate() - 7)
+
+let nowDate = ("0" + today.getDate()).slice(-2);
+let nowMonth = today.getMonth() + 1;
+nowMonth = ("0" + nowMonth).slice(-2)
+let nowYear = today.getFullYear();
+
+let oldDate = ("0" + lastWeek.getDate()).slice(-2);
+let oldMonth = lastWeek.getMonth() + 1;
+oldMonth = ("0" + oldMonth).slice(-2)
+let oldYear = lastWeek.getFullYear();
+
+let fromDate = oldYear + "-" + oldMonth + "-" + oldDate
+let toDate = nowYear + "-" + nowMonth + "-" + nowDate
+
+newsapi.v2.everything({
+    qInTitle: 'crypto AND (ethereum OR litecoin) NOT bitcoin',
+    from: fromDate,
+    to: toDate,
+    language: 'en',
+    sortBy: 'relevancy',
+}).then(response => {
+    console.log(response);
+    article = response;
+});
+
